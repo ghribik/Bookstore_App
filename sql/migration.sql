71c27279a66cb9d48a929d079fa0c655a0881497
ghribik <g.hribik@gmail.com>
2023-01-31 14:06:52 -0800

    last commit before deploying to aws ec2 instance

--- sql/migration.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS books (
-    id serial NOT NULL,
-    title VARCHAR(50),
-    author VARCHAR(50),
-    cover TEXT,
-    isbn BIGINT,
-    price MONEY,
-    PRIMARY KEY (id)
-);
